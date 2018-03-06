73c57b5997d3f1b9c9fc4dc0dcb7a2f23a710398
smartjinyu <smartjinyu@gmail.com>
2018-03-06 12:40:00 +0800

    finish problem 412

---
+++ 412_Fizz_Buzz.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+vector<string> fizzBuzz(int n) {
+    vector<string> ans;
+    for (int i = 1; i <= n; i++){
+        if (i % 15 == 0){
+            ans.push_back("FizzBuzz");
+        } else if (i % 3 == 0){
+            ans.push_back("Fizz");
+        } else if (i % 5 == 0){
+            ans.push_back("Buzz");
+        } else{
+            ans.push_back(to_string(i));
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<string> ans = fizzBuzz(15);
+    for (int i = 0; i < ans.size(); i++){
+        printf("%s\n",ans[i]);
+    }
+    return 0;
+}
