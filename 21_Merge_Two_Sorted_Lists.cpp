5e67a6fa21bac1353536aef89ac01b96439656dc
smartjinyu <smartjinyu@gmail.com>
2018-03-07 19:47:11 +0800

    finish problem 21

---
+++ 21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// This solution is in reference to the discuss in LeetCode
+Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if(l1 == NULL){
+        return l2;
+    } else if(l2 == NULL){
+        return l1;
+    } else {
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+}
+
+int main(){
+    return 0;
+}
