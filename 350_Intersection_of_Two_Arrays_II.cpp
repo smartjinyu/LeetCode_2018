13696055f8d790fdc4d9b9b4d514fc2135c505c4
smartjinyu <smartjinyu@gmail.com>
2018-03-18 14:50:06 +0800

    finish problem 350

---
+++ 350_Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> res;
+    if(nums1.size() == 0 || nums2.size() == 0) return res;
+    unordered_map<int,int> um;
+    for(int num : nums1) um[num]++;
+    for(int num : nums2){
+        if(um[num] != 0){
+            res.push_back(num);
+            um[num]--;
+        }
+    }
+    return res;
+
+}
+int main(){
+    return 0;
+}
