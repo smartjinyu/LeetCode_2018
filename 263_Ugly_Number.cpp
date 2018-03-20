f85c656b336a69ee4e77083e8fad9f87f3b91cf0
smartjinyu <smartjinyu@gmail.com>
2018-03-20 12:43:27 +0800

    finish problem 263

---
+++ 263_Ugly_Number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// stupid problem, <= 0 are all false
+bool isUgly(int num) {
+    if(num <= 0) return false;
+    while(num % 2 == 0) num /= 2;
+    while(num % 3 == 0) num /= 3;
+    while(num % 5 == 0) num /= 5;
+    return num == 1;
+}
+int main(){
+    return 0;
+}
