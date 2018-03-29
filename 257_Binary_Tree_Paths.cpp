326f2fd81352ffc3ad49bb6cc0771f7833ae3bc9
smartjinyu <smartjinyu@gmail.com>
2018-03-29 20:04:31 +0800

    finish problem 257

---
+++ 257_Binary_Tree_Paths.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+void getPath(TreeNode* node, vector<int>& cur, vector<string>& ans){
+    if(node->left == NULL && node->right == NULL){
+        cur.push_back(node->val);
+        if(cur.size() == 0){
+            return;
+        }else if(cur.size() == 1){
+            ans.push_back(to_string(cur[0]));
+        }else{
+            string str;
+            str.append(to_string(cur[0]));
+            for(int i = 1; i < cur.size(); i++){
+                str.append("->");
+                str.append(to_string(cur[i]));
+            }
+            ans.push_back(str);
+        }
+    }else if(node->left == NULL){
+        cur.push_back(node->val);
+        getPath(node->right,cur,ans);
+    }else if(node->right == NULL){
+        cur.push_back(node->val);
+        getPath(node->left,cur,ans);
+    }else{
+        cur.push_back(node->val);
+        vector<int> cur1 = cur;
+        getPath(node->left,cur,ans);
+        getPath(node->right,cur1,ans);
+    }
+}
+vector<string> binaryTreePaths(TreeNode* root) {
+    vector<string> ans;
+    if(root == NULL) return ans;
+    vector<int> cur;
+    getPath(root,cur,ans);
+    return ans;
+}
+int main(){
+    return 0;
+}
